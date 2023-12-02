cdf53c977bf5d7f858294325964cc2ddb2c6dacb
LixvYang <2690688423@qq.com>
2023-12-02 18:10:04 +0800

    update

--- examples/main.go
+++ examples/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/lixvyang/rum-sdk-go/sdk"
 	"github.com/lixvyang/rum-sdk-go/sdk/base"
+	"github.com/lixvyang/rum-sdk-go/service/fullnode"
 )
 
 func main() {
-	cli := sdk.New(&base.Configuration{
+	cli := fullnode.New(&base.Configuration{
 		BasePath: "http://127.0.0.1:8000",
 	})
 	resp, _, err := cli.GroupsApi.ApiV1GroupGroupIdGet(context.Background(), "c7e9b833-e60a-4901-9abc-c8ec5b15a7e6")
